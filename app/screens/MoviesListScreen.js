bcc8e5e2b20f69c179a9c8895bdbd65402d2d76f
Sushant Devkota <sushant.devkota@braindigit.com>
2016-05-25 17:09:12 +0545

    styles added

--- app/screens/MoviesListScreen.js
+++ app/screens/MoviesListScreen.js
@@ -72,5 +72,33 @@ class MoviesListScreen extends Component{
       </View>
     );
   }
-
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center'
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
